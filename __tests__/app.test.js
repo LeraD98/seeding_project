9a58f5562653bb33ce2e1f7343713df6bec886fc
Lera Dunning <lera@Mac.lan>
2025-04-29 12:11:52 +0100

    GET /api/articles/:article_id

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,15 +1,46 @@
 const endpointsJson = require("../endpoints.json");
 /* Set up your test imports here */
-
+const request = require("supertest");
+const app = require("../app");
 /* Set up your beforeEach & afterAll functions here */
 
 describe("GET /api", () => {
-  test.skip("200: Responds with an object detailing the documentation for each endpoint", () => {
+  test("200: Responds with an object detailing the documentation for each endpoint", () => {
     return request(app)
       .get("/api")
       .expect(200)
       .then(({ body: { endpoints } }) => {
         expect(endpoints).toEqual(endpointsJson);
       });
+  });
+});
+
+describe('GET /api/topics', () => {
+  let response;
+
+  beforeAll(async () => {
+    const { body } = await request(app).get('/api/topics');
+    response = body;
+  });
+
+  test('responds with status 200', async () => {
+    const res = await request(app).get('/api/topics');
+    expect(res.status).toBe(200);
+  });
+
+  test('responds with an array of topics', () => {
+    expect(response.topics).toBeInstanceOf(Array);
+    expect(response.topics.length).toBeGreaterThan(0); // optional
+  });
+
+  test('each topic has a slug and description', () => {
+    response.topics.forEach((topic) => {
+      expect(topic).toEqual(
+        expect.objectContaining({
+          slug: expect.any(String),
+          description: expect.any(String),
+        })
+      );
+    });
   });
 });
